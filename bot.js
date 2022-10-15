26fddbc215a52d54e9fe5e509260d2a9d2d5f7a6
RuneDevroe <r0846367@student.vives.be>
2022-10-15 17:14:33 +0200

    beefar

--- bot.js
+++ bot.js
@@ -1,18 +1,33 @@
-require("dotenv").config()
+const { Client, Intents } = require('discord.js');
+const command = require('./command');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require('./config.json');
 
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.on('ready', ()=> {
+    console.log('The client is ready!')
 
-client.on('ready', () => {
-  console.log("bot is running")
-})
+    command(client, 'ping', (message) => {
+        message.channel.send('Pong!')
+    })
 
-client.on('message', msg => {
-    console.log("testing ping")
-    if(msg.content === 'ping'){
-        console.log("ping")
-        msg.channel.send('Pong!')
-    }
+    command(client, 'hoe dik is nico', (message) => {
+        message.channel.send('zeer dik')
+    })
+
+    command(client, 'servers', (message) =>{
+        client.guilds.cache.forEach((guild =>{
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
+        }))
+    })
+    command(client, 'status', (message) =>{
+        const content = message.content.replace('!status ', '')
+        client.user.setPresence({
+            activity:{
+                name: content,
+                type: 0
+            }
+        })
+    })
 })
 
 client.login(process.env.BOT_TOKEN)
