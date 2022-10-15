86fdf9543c1f79072f62fffc84a92b70bde219e2
RuneDevroe <r0846367@student.vives.be>
2022-10-15 16:38:08 +0200

    ping command

---
+++ bot.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log("bot is running");
+});
+
+client.on('message', msg => {
+    if(msg.content === "ping" ){
+        msg.channel.send("Pong!")
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
